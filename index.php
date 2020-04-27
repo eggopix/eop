f6f13212f3616c7b5250175b6f0db707d1e0cc59
eggopix <nicolasdenoel@icloud.com>
2020-04-27 16:00:52 +0200

    basic structure ok

--- index.php
+++ index.php
@@ -7,64 +7,14 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="fullpage">
-        <header>
-            1
-        </header>
-        <nav>
-            2
-        </nav>
-        <div class="intro">
-            3   
-        </div>
-        <div class="progress">
-            4
-        </div>
-        <div class="last-article">
-            5
-        </div>
-        <div class="best-links">
-            6
-        </div>
-        <div class="photo-video">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
-        </div>
-        <footer>
-            8
-        </footer>
+    <header>
+        <div class="logo">LOGO</div>
+        <div class="menu">MENU</div>
+        <div class="login">LOGIN</div>
+        <nav>NAVIGATION</nav>
+        <main>MAIN</main>
+        <aside>ASIDE</aside>
+        <footer>FOOTER</footer>
     </div>
 </body>
 </html>
