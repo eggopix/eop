338d5c250e7c3df0f9e363db26b51da002263d87
eggopix <nicolasdenoel@icloud.com>
2020-04-27 13:50:00 +0200

    create a first grid layout

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="fullpage">
+        <header>
+            1
+        </header>
+        <nav>
+            2
+        </nav>
+        <div class="intro">
+            3   
+        </div>
+        <div class="progress">
+            4
+        </div>
+        <div class="last-article">
+            5
+        </div>
+        <div class="best-links">
+            6
+        </div>
+        <div class="photo-video">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+            <img src="https://picsum.photos/id/1015/50/50" alt"pix-50-50">
+        </div>
+        <footer>
+            8
+        </footer>
+    </div>
+</body>
+</html>
