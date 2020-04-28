8d445bb1334d2f0a2218fd9056931539dd30834a
eggopix <nicolasdenoel@icloud.com>
2020-04-28 17:40:34 +0200

    add articles /end header-footer-nav.Ready for v0.2

--- sql.php
+++ sql.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SQL</title>
+    <link rel="icon" href="images/favicon.ico">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/16f591d449.js" crossorigin="anonymous"></script>
 </head>
