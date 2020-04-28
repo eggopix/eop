162612b04ca33fcba3e7543bffdac96dea323891
eggopix <nicolasdenoel@icloud.com>
2020-04-28 15:31:44 +0200

    navigation color - header and footer mobile ok

--- header.php
+++ header.php
@@ -11,11 +11,12 @@
 </div>
 <nav>
     <ul>
-        <li><a href="html.php"><i class="fab fa-html5"></i></a></li>
-        <li><a href="css.php"><i class="fab fa-css3-alt"></i></a></li>
-        <li><a href="php.php"><i class="fab fa-php"></i></a></li>
-        <li><a href="sql.php"><i class="fas fa-database"></i></a></li>
-        <li><a href="misc.php"><i class="fas fa-book"></i></a></li>
-        <li><a href="contact.php"><i class="fas fa-at"></i></a></li>
+        <li class="html"><a href="html.php"><i class="fab fa-html5"></i></a></li>
+        <li class="css"><a href="css.php"><i class="fab fa-css3-alt"></i></a></li>
+        <li class="php"><a href="php.php"><i class="fab fa-php"></i></a></li>
+        <li class="sql"><a href="sql.php"><i class="fas fa-database"></i></a></li>
+        <li class="misc"><a href="misc.php"><i class="fas fa-book"></i></a></li>
+        <li class="pixdeo"><a href="pixdeo.php"><i class="fas fa-photo-video"></i></a></li>
+        <li class="contact"><a href="contact.php"><i class="fas fa-at"></i></a></li>
     </ul>
 </nav>
