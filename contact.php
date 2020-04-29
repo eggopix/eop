5c20f9ddffff3546ab72f2184cc32a0aef8907d1
eggopix <nicolasdenoel@icloud.com>
2020-04-29 16:27:47 +0200

    little change to menu overlay color

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php include('head.php'); ?>
-<body>
-    <div class="page">
-        <?php include('header.php'); ?>
-        <?php include('main.php'); ?>
-        <?php include('footer.php'); ?>
-    </div>
-</body>
+  <body>
+      <div class="page">
+          <?php include('header.php'); ?>
+          <?php include('main.php'); ?>
+          <?php include('footer.php'); ?>
+      </div>
+  </body>
 </html>
